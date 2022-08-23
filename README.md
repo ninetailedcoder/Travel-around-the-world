cbf20f27ee78a6d50f0b80c4a429467f4abe39e7
ninetailedcoder <torydemaio@gmail.com>
2022-08-23 15:04:22 -0500

    providing better detailed instructions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Travel-around-the-world
+
+for best results preform following steps 
+1.) Save own photo to the appropriate file location 
+
+2.) Edit variable to your appropriate file names.
--- travel app.py
+++ travel app.py
@@ -1,6 +1,9 @@
 from PIL import Image
 
-me = Image.open('travel photo.png')
-bg= Image.open('travel three.jpg')
+#pastes one file photo onto another 
+
+#edit variables me and bg to the name of your files 
+me = Image.open('')
+bg= Image.open('')
 bg.paste(me, (0, 0), me)
 bg.show()
